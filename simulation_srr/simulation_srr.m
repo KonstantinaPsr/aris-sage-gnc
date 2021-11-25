fa50db4e83180abb719f9fab0a61b97472dd9e29
eelbir <emre-elbir@outlook.com>
2021-11-25 15:26:10 +0100

    Finished the first simple simulation giving the gravitation acting on the CoM of the payload

--- simulation_srr/simulation_srr.m
+++ simulation_srr/simulation_srr.m
@@ -8,12 +8,13 @@ height = unit*0.1; % points along x axis
 length = 0.1; % points along y axis
 width = 0.1; % points along z axis
 
+
 volume = length*width*height; % [m^3]
-mass_max = unit*1.33; % [kg], 1.33kg is the max weight of a cubesat unit
-I_x = 1/12*mass*(width^2+length^2); % [kg*m^2], inertia around x-axis
-I_y = 1/12*mass*(width^2+height^2); % [kg*m^2], inertia around y-axis
-I_z = 1/12*mass*(length^2+height^2); % [kg*m^2], inertia around z-axis
-I = [I_x 0 0; 0 I_y 0; 0 0 I_z]; % [kg*m^2], inertia tensor
+% mass = unit*1.33; % [kg], 1.33kg is the max weight of a cubesat unit
+% I_x = 1/12*mass*(width^2+length^2); % [kg*m^2], inertia around x-axis
+% I_y = 1/12*mass*(width^2+height^2); % [kg*m^2], inertia around y-axis
+% I_z = 1/12*mass*(length^2+height^2); % [kg*m^2], inertia around z-axis
+% I = [I_x 0 0; 0 I_y 0; 0 0 I_z]; % [kg*m^2], inertia tensor
 omega = [0; 0; 0]; % [1/s], rotational speed of satellite
 
 % the geometrical reference frame is placed at the exact center of the cubesat
@@ -31,5 +32,14 @@ d_CS_CSP = r_CoM_satellite - r_CoM_sat_platform; % [m]
 % Intertia around satellite CoM
 I_satellite = mass_payload*d_CS_CPL^2 + mass_sat_platform*d_CS_CSP^2; % [kg*m^2]
 
-% Angular momentum provided by the reaction wheel
+% Angular momentum provided by the reaction wheel (provided from rocket
+% lab)
 L_wheel = 0.001; % [kg*m^2/s] 
+% resulting rotational speed around the z-axis
+omega_z = L_wheel/I_satellite; % [1/s]
+disp(omega_z);
+% velocity at the payload CoM
+v_payload = omega_z*d_CS_CPL; % [m/s]
+F_payload = mass_payload*v_payload^2/d_CS_CPL; % [N]
+g_payload = F_payload/mass_payload; % [m/s^2]
+disp(g_payload);
