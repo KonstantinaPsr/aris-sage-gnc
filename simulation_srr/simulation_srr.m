515491c65863a3f9994b5214968e72efdae8d207
eelbir <emre-elbir@outlook.com>
2021-12-02 17:46:48 +0100

    Refined the simulation, created two new plots. The first plot shows the relation of the acceleration at the payload as a function of the angular velocity around the rotation axis which itself depends on the angular momentum supplied by the reaction wheel. In this case the satellite mass is fixed to 4kg and the weight distribution stays constant. The second plot shows the effect of a increasing the payload mass while keeping the satellite platform mass constant. The acceleration at the CoM of the payloas and the angular velovity around the rotational axis is plotted as a function of the payload mass and the relation of payload mass over the mass of the satellite platform (mass of satellite without payload mass)

--- simulation_srr/simulation_srr.m
+++ simulation_srr/simulation_srr.m
@@ -22,8 +22,10 @@ omega = [0; 0; 0]; % [1/s], rotational speed of satellite
 %% Compute rotational speed and gravity acting on payload
 
 % the geometrical reference frame is placed at the exact center of the cubesat
-mass_payload = 1.2; % [kg]
-mass_sat_platform = 2.66; % [kg]
+mass_payload = 1.33; % based on the maximum weight for a cubesat unit [kg]
+mass_sat_platform = 2.66; % 2 cubesat units with the maximum weight [kg]
+%r_CoM_payload = 0.05:0.005:0.14; % [m]
+%r_CoM_sat_platform = -0.05:-0.005:-0.14; % [m] 
 r_CoM_payload = 0.1; % [m]
 r_CoM_sat_platform = -0.1; % [m] 
 % center of mass of whole payload in the geometric reference frame
@@ -38,24 +40,146 @@ I_satellite = mass_payload*d_CS_CPL^2 + mass_sat_platform*d_CS_CSP^2; % [kg*m^2]
 
 % Angular momentum provided by the reaction wheel (provided from rocket
 % lab)
-L_wheel = 0.001; % [kg*m^2/s] 
+% L_wheel = 0.01; % [kg*m^2/s] 
 % resulting rotational speed around the z-axis
-omega_z = L_wheel/I_satellite; % [1/s]
-disp(omega_z);
+% omega_z = L_wheel/I_satellite; % [1/s]
+% disp(omega_z);
 % velocity at the payload CoM
-v_payload = omega_z*d_CS_CPL; % [m/s]
-F_payload = mass_payload*v_payload^2/d_CS_CPL; % [N]
+% v_payload = omega_z*d_CS_CPL; % [m/s]
+% F_payload = mass_payload*v_payload^2/d_CS_CPL; % [N]
+% g_payload = F_payload/mass_payload; % [m/s^2]
+% disp(g_payload);
+
+%% Effect of the angular momentum of the actuator on the rotational velocity and the artificial gravity
+% The minimum and maximum angualr momentum values are based on the reaction
+% wheel developed by rocketlab which make sense for our case and still fit
+% inside our 3U CubeSat
+% The lowest value is from the 3mNms RW-0.003 (33.5mm x 33.5mm x 17mm)
+% The largest value is from the 30mNms RW-0.03 (50mm x 50 mm x 40 mm)
+
+mass = 1.33*3; % weight of the satellite is based on the maximum weight typical for a satellite [kg]
+L_wheel_range = 0.003:0.0001:0.03; % range for the angular momentum [Nms]
+omega_z_range = L_wheel_range/I_satellite; % angular velocity around main axis [1/s]
+% velocity at the payload CoM
+v_payload = omega_z_range*d_CS_CPL; % [m/s]
+% centripetal force at CoM of payload
+F_payload = mass_payload*v_payload.^2/d_CS_CPL; % [N]
+% aritficial gravity acting at CoM of payload
 g_payload = F_payload/mass_payload; % [m/s^2]
-disp(g_payload);
+%disp(g_payload);
 
+figure(1);
+t = tiledlayout(1,1);
+ax1 = axes(t);
+plot(ax1, omega_z_range, g_payload);
+plot_title = title({'Artificial gravity for a fixed satellite weight of 4kg';'';''});
+plot_title.FontSize = 14;
+xlabel('Angular velocity around rotation axis [1/s]');
+ylabel('Acceleration at CoM of payload [m/s^2]');
+ax2 = axes(t);
+plot(ax2, L_wheel_range, g_payload);
+xlabel('Angular momentum provided by reaction wheel [Nms]')
+ax2.XAxisLocation = 'top';
+x0=10;
+y0=10;
+width=1100;
+height=800;
+set(gcf,'position',[x0,y0,width,height])
 
+% t = tiledlayout(1,1);
+% ax1 = axes(t);
+% plot(ax1,x1,y1,'-r')
+% ax1.XColor = 'r';
+% ax1.YColor = 'r';
+% ax2 = axes(t);
+% plot(ax2,x2,y2,'-k')
+% ax2.XAxisLocation = 'top';
+% ax2.YAxisLocation = 'right';
+% ax2.Color = 'none';
+% ax1.Box = 'off';
+% ax2.Box = 'off';
 
+%% Weight relation between satellite platform and payload
+% the geometrical reference frame is placed at the exact center of the cubesat
+mass_payload = 0.5:0.025:2; % weight of payload [kg]
+mass_sat_platform = 2; % weight of satellite platform [kg]
+weight_ratio = mass_payload/mass_sat_platform;
+%r_CoM_payload = 0.05:0.005:0.14; % [m]
+%r_CoM_sat_platform = -0.05:-0.005:-0.14; % [m] 
+r_CoM_payload = 0.1; % [m]
+r_CoM_sat_platform = -0.1; % [m] 
+% center of mass of whole payload in the geometric reference frame
+r_CoM_satellite = (r_CoM_payload.*mass_payload + r_CoM_sat_platform.*mass_sat_platform) ./ (mass_payload + mass_sat_platform); % [m]
+disp(r_CoM_satellite);
+% distance from satellite CoM to payload CoM
+d_CS_CPL = r_CoM_payload - r_CoM_satellite; % [m]
+% distance from satellite CoM to satellite platform CoM
+d_CS_CSP = r_CoM_satellite - r_CoM_sat_platform; % [m]
+
+% Intertia around satellite CoM
+I_satellite = mass_payload.*d_CS_CPL.^2 + mass_sat_platform.*d_CS_CSP.^2; % [kg*m^2]
+
+L_wheel_range = 0.01; % range for the angular momentum [Nms]
+omega_z_range = L_wheel_range./I_satellite; % angular velocity around main axis [1/s]
+% velocity at the payload CoM
+v_payload = omega_z_range.*d_CS_CPL; % [m/s]
+% centripetal force at CoM of payload
+F_payload = mass_payload.*v_payload.^2./d_CS_CPL; % [N]
+% aritficial gravity acting at CoM of payload
+g_payload = F_payload./mass_payload; % [m/s^2]
+
+figure(2);
+t = tiledlayout(1,1);
+ax1 = axes(t);
+plot(ax1, mass_payload, g_payload, '-k');
+ax1.XColor = 'k';
+ax1.YColor = 'k';
+plot_title = title({'Artificial gravity for a satellite platform with a weight of 2kg';'';''});
+plot_title.FontSize = 14;
+xlabel('Payload mass [kg]');
+y = ylabel('Acceleration at CoM of payload [m/s^2]');
+ax2 = axes(t);
+plot(ax2, weight_ratio, omega_z_range, '-b');
+ax2.XColor = 'b';
+ax2.YColor = 'b';
+xlabel('m_{payload}/m_{platform}');
+ylabel('Angular velocity around rotation axis [1/s]')
+ax2.XAxisLocation = 'top';
+ax2.YAxisLocation = 'right';
+ax2.Color = 'none';
+ax1.Box = 'off';
+ax2.Box = 'off';
+x0=10;
+y0=10;
+width=1100;
+height=800;
+set(gcf,'position',[x0,y0,width,height])
+
+% figure(3);
+% plot(mass_payload, r_CoM_satellite);
+% xlabel('Payload mass [kg]');
+% ylabel('Position of satellite CoM [m]');
+% 
+% figure(4);
+% plot(mass_payload, I_satellite);
+% xlabel('Payload mass [kg]');
+% ylabel('Intertia of satellite [kg*m^2]');
+% 
+% figure(5);
+% plot(mass_payload, omega_z_range);
+% xlabel('Payload mass [kg]');
+% ylabel('Angular velocity [1/s]');
+% 
+% figure(6);
+% plot(mass_payload, g_payload);
+% xlabel('Payload mass [kg]');
+% ylabel('Acceleration [m/s^2]');
+% 
+% disp('-----------------------------------------------------------------')
+% disp(g_payload)
+% disp(omega_z_range)
 %% Compute needed Power
-% Calculations based on the 
-
-%% TODO
-% Gravity to torque plot
-% Power to torque plot
-% Power to gravity plot
-% Angular Momentum to gravity plot
-% Actuator Comparison
+% The values are based on the three reaction wheel types which would fit
+% into our satellite
+L_wheels = [1 0.01 0.02]; % Angula momentum [Nms]
+P_wheels = [0 1.05 1.8]; % Power consumption [W]
